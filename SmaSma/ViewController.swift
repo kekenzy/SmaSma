33ac7a88504c07f3c80d1421f04407cf453a2cd7
ADK114019 <ADK114019@adk114019-no-imac.local>
2016-12-27 14:50:54 +0900

    Initial Commit

---
+++ SmaSma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmaSma
+//
+//  Created by ADK114019 on 2016/12/27.
+//  Copyright © 2016年 ADK114019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
